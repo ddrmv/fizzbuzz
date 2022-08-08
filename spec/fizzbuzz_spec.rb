be0dc8411f2c3ae3989a1e48a0af56255417c72e
D. Dramchev <ddramchev@gmail.com>
2022-08-08 11:05:03 +0100

    Implement test and code in full
    Co-authored-by: Guillermina Lorenzo <glorenzo2001@gmail.com>

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'return "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'return "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'return "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'return 2 when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
+  end 
+end
